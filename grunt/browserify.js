c15ebc7cfd6bc66a1354f9c9d1f9c39ff2629698
Pascal Birchler <hello@pascalbirchler.ch>
2015-03-18 22:01:54 +0100

    Configure browserify task

---
+++ grunt/browserify.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dist: {
+    files  : {
+      'js/build/feedback.js': ['js/src/feedback.js']
+    },
+    options: {
+      transform: ['node-underscorify'],
+      debug    : true,
+      external : ['jquery', 'underscore', 'backbone']
+    }
+  }
+};
