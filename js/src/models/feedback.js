a77a927fb05ab3956bc127c95a146ff1fec56397
Pascal Birchler <hello@pascalbirchler.ch>
2015-11-21 19:42:47 +0100

    Simplify app view, move cookie check to model

--- js/src/models/feedback.js
+++ js/src/models/feedback.js
@@ -1,5 +1,11 @@
 'use strict';
 
-var Feedback = Backbone.Model.extend( {} );
+var Feedback = Backbone.Model.extend(
+	{
+		initialize: function () {
+			this.set( 'doNotShowInfoAgain', document.cookie.indexOf( 'user_feedback_do_not_show_again' ) >= 0 );
+		}
+	}
+);
 
 module.exports = Feedback;
--- js/src/views/app.js
+++ js/src/views/app.js
@@ -6,30 +6,24 @@ var Button = require( 'views/button' ),
 var App = wp.Backbone.View.extend(
 	{
 		el: '#user-feedback-container',
-		inProgress: false,
 
 		initialize: function () {
-			this.views.set( '.user-feedback-sub-view', new Button( { model: this.model } ) );
-
-			this.listenTo( this.model, 'change:inProgress', this.changeProgress );
-
-			this.model.set( 'doNotShowInfoAgain', document.cookie.indexOf('user_feedback_do_not_show_again') >= 0 );
+			this.listenTo( this.model, 'change:inProgress', this.render );
 		},
 
 		render: function () {
-			if ( !this.inProgress ) {
+			if ( !this.model.get( 'inProgress' ) ) {
 				// Button view.
 				this.views.set( '.user-feedback-sub-view', new Button( { model: this.model } ) );
 			} else {
 				// Bubble view that contains the individual steps.
 				this.views.set( '.user-feedback-sub-view', new Bubble( { model: this.model } ) );
 			}
-		},
 
-		changeProgress: function () {
-			this.inProgress = !this.inProgress;
-			this.render();
-		}
+			this.delegateEvents();
+
+			return this;
+		},
 	}
 );
 
