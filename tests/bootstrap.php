983c49490b0ff42eb5c4af5a433717b836ec2fff
Pascal Birchler <hello@pascalbirchler.ch>
2016-04-28 12:35:22 +0200

    Tests: Add some PHPUnit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,27 +1,16 @@
 <?php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../user-feedback.php';
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-
-class User_Feedback_TestCase extends WP_UnitTestCase {
-	function plugin() {
-		return User_Feedback_Plugin::get_instance();
-	}
-
-	function set_post( $key, $value ) {
-		$_POST[$key] = $_REQUEST[$key] = addslashes( $value );
-	}
-
-	function unset_post( $key ) {
-		unset( $_POST[$key], $_REQUEST[$key] );
-	}
-}
---
+++ tests/phpunit/tests/DataProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Required\User_Feedback\Tests;
+
+class DataProvider extends \WP_UnitTestCase {
+	function _filter_user_feedback_support_data( $data ) {
+		return [
+			'foo' => 'bar',
+			'bar' => [ 'baz' ],
+		];
+	}
+
+	function test_should_return_the_ajax_url() {
+		$data = ( new \Required\User_Feedback\DataProvider() )->get_data();
+
+		$this->assertEquals( admin_url( 'admin-ajax.php' ), $data['ajax_url'] );
+	}
+
+	function test_should_return_the_site_language() {
+		$data = ( new \Required\User_Feedback\DataProvider() )->get_data();
+
+		$this->assertEquals( get_bloginfo( 'language' ), $data['language'] );
+	}
+
+	function test_should_return_anonymous_user_data() {
+		$data = ( new \Required\User_Feedback\DataProvider() )->get_data();
+
+		$this->assertEqualSets( [
+			'logged_in' => false,
+			'name'      => __( 'Anonymous', 'user-feedback' ),
+			'email'     => false,
+		], $data['user'] );
+	}
+
+	function test_should_return_logged_in_user_data() {
+		$current_user = get_current_user_id();
+
+		wp_set_current_user( $this->factory->user->create( [
+			'display_name' => 'John Doe',
+			'user_email'   => 'johndoe@required.ch',
+			'role'         => 'administrator',
+		] ) );
+
+		$data = ( new \Required\User_Feedback\DataProvider() )->get_data();
+
+		wp_set_current_user( $current_user );
+
+		$this->assertEqualSets( [
+			'logged_in' => true,
+			'name'      => 'John Doe',
+			'email'     => 'johndoe@required.ch',
+		], $data['user'] );
+	}
+
+	function test_should_return_third_party_data() {
+		add_filter( 'user_feedback_support_data', [ $this, '_filter_user_feedback_support_data' ] );
+		$data = ( new \Required\User_Feedback\DataProvider() )->get_data();
+		remove_filter( 'user_feedback_support_data', [ $this, '_filter_user_feedback_support_data' ] );
+
+		$this->assertEquals( [
+			'foo' => 'bar',
+			'bar' => [ 'baz' ],
+		], $data['third_party'] );
+	}
+
+	function test_should_return_info_for_anonymous_user() {
+		$data = ( new \Required\User_Feedback\DataProvider() )->get_data();
+
+		$this->assertEqualSets( [
+			'logged_in' => false,
+			'name'      => __( 'Anonymous', 'user-feedback' ),
+			'email'     => false,
+		], $data['user'] );
+	}
+}
